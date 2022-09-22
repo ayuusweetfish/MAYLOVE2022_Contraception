db5f6e8971e92df65ab501fda6ff80312f2bc1e0
Shiqing <shiqing-thu18@yandex.com>
2022-09-22 14:43:39 +0800

    修复数字无法显示的问题

--- font_subset.sh
+++ font_subset.sh
@@ -2,4 +2,4 @@
 
 pyftsubset res/1574853606.ttf \
   --output-file=res/1574853606_subset.ttf \
-  --text=`cat *.lua | perl -CIO -pe 's/[\p{ASCII} \N{U+2500}-\N{U+257F}]//g'`
+  --text=0123456789`cat *.lua | perl -CIO -pe 's/[\p{ASCII} \N{U+2500}-\N{U+257F}]//g'`
